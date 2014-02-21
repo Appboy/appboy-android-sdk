6a3425ce1818ad6a25e6b8734ba5e93be8a3eded
TobiasO <tobiaso@planeto.com>
2014-02-21 11:39:36 +0100

    SDK < 11 compatibility

--- android-sdk-ui/src/com/appboy/ui/actions/ActivityAction.java
+++ android-sdk-ui/src/com/appboy/ui/actions/ActivityAction.java
@@ -12,18 +12,45 @@ public final class ActivityAction implements IAction {
   private final Intent mIntent;
 
   public ActivityAction(Intent intent) {
+    mIntent = intent;
   }
 
   public ActivityAction(Context ctx, Uri uri) {
     mIntent.setClassName(ctx, uri.getHost());
-    for(String key : uri.getQueryParameterNames()) {
-      mIntent.putExtra(key, uri.getQueryParameter(key));
-    }
+    setQueryParameters(uri);
+  }
     mIntent = intent;
   }
 
   @Override
   public void execute(Context context) {
     context.startActivity(mIntent);
+  }
+
+  /** Mostly copy paste from SDK 11 **/
+  private void setQueryParameters(Uri uri) {
+    if (uri.isOpaque()) {
+      throw new UnsupportedOperationException("This isn't a hierarchical URI.");
+    }
+
+    String query = uri.getEncodedQuery();
+    if (query == null) {
+      return;
+    }
+
+    int start = 0;
+    do {
+      int next = query.indexOf('&', start);
+      int end = (next == -1) ? query.length() : next;
+
+      int separator = query.indexOf('=', start);
+      if (separator <= end && separator >= 0) {
+        String key = query.substring(start, separator);
+        String value = query.substring(separator + 1, end);
+        mIntent.putExtra(Uri.decode(key), Uri.decode(value));
+      }
+      // Move start to end of name.
+      start = end + 1;
+    } while (start < query.length());
   }
 }
