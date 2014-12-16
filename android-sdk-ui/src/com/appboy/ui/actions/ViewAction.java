775e769e2403f73cef52e65454d3bad8e33de81a
Daniel Tashjian <daniel@creeaaakk.com>
2014-12-16 14:37:45 -0500

    Added support for giving a bundle to ActionFactory.

--- android-sdk-ui/src/com/appboy/ui/actions/ViewAction.java
+++ android-sdk-ui/src/com/appboy/ui/actions/ViewAction.java
@@ -3,6 +3,7 @@ package com.appboy.ui.actions;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 
 /**
  * Fires an android.intent.action.VIEW action with the given URI.
@@ -10,9 +11,13 @@ import android.net.Uri;
 public class ViewAction implements IAction {
   private final Intent mIntent;
 
-  public ViewAction(Uri uri) {
+  public ViewAction(Uri uri, Bundle extras) {
     mIntent = new Intent(Intent.ACTION_VIEW);
     mIntent.setData(uri);
+
+    if (extras != null) {
+      mIntent.putExtras(extras);
+    }
   }
 
   @Override
