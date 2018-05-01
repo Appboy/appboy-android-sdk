be18c102a9cc4dfaffa52c0aad9515e55264d7bf
Martin Gonzalez <martin.gonzalez@etermax.com>
2018-05-01 19:49:40 -0300

    Revert InAppMessage.java to it's original commit

--- android-sdk-unity/src/com/appboy/unity/utils/InAppMessageUtils.java
+++ android-sdk-unity/src/com/appboy/unity/utils/InAppMessageUtils.java
@@ -10,44 +10,44 @@ import com.appboy.models.MessageButton;
 import com.appboy.support.AppboyLogger;
 
 public class InAppMessageUtils {
-    private static final String TAG = AppboyLogger.getAppboyLogTag(InAppMessageUtils.class);
+  private static final String TAG = AppboyLogger.getAppboyLogTag(InAppMessageUtils.class);
 
-    public static IInAppMessage inAppMessageFromString(Context context, String messageJSONString) {
-        return Appboy.getInstance(context).deserializeInAppMessageString(messageJSONString);
-    }
+  public static IInAppMessage inAppMessageFromString(Context context, String messageJSONString) {
+    return Appboy.getInstance(context).deserializeInAppMessageString(messageJSONString);
+  }
 
-    public static void logInAppMessageClick(IInAppMessage inAppMessage) {
-        if (inAppMessage != null) {
-            inAppMessage.logClick();
-        } else {
-            Log.e(TAG, "The in-app message is null, Not logging in-app message click.");
-        }
+  public static void logInAppMessageClick(IInAppMessage inAppMessage) {
+    if (inAppMessage != null) {
+      inAppMessage.logClick();
+    } else {
+      Log.e(TAG, "The in-app message is null, Not logging in-app message click.");
     }
+  }
 
-    public static void logInAppMessageButtonClick(IInAppMessage inAppMessage, int buttonId) {
-        if (inAppMessage == null) {
-            Log.e(TAG, "The in-app message is null. Not logging in-app message button click.");
-            return;
-        }
-        if (inAppMessage instanceof IInAppMessageImmersive) {
-            IInAppMessageImmersive inAppMessageImmersive = (IInAppMessageImmersive)inAppMessage;
-            for (MessageButton button : inAppMessageImmersive.getMessageButtons()) {
-                if (button.getId() == buttonId) {
-                    inAppMessageImmersive.logButtonClick(button);
-                    break;
-                }
-            }
-        } else {
-            Log.e(TAG, "The in-app message %s isn't an instance of "
-                    + "InAppMessageImmersive. Not logging in-app message button click.");
-        }
+  public static void logInAppMessageButtonClick(IInAppMessage inAppMessage, int buttonId) {
+    if (inAppMessage == null) {
+      Log.e(TAG, "The in-app message is null. Not logging in-app message button click.");
+      return;
     }
+    if (inAppMessage instanceof IInAppMessageImmersive) {
+      IInAppMessageImmersive inAppMessageImmersive = (IInAppMessageImmersive)inAppMessage;
+      for (MessageButton button : inAppMessageImmersive.getMessageButtons()) {
+        if (button.getId() == buttonId) {
+          inAppMessageImmersive.logButtonClick(button);
+          break;
+        }
+      }
+    } else {
+      Log.e(TAG, "The in-app message %s isn't an instance of "
+          + "InAppMessageImmersive. Not logging in-app message button click.");
+    }
+  }
 
-    public static void logInAppMessageImpression(IInAppMessage inAppMessage) {
-        if (inAppMessage != null) {
-            inAppMessage.logImpression();
-        } else {
-            Log.e(TAG, "The in-app message is null, Not logging in-app message impression.");
-        }
+  public static void logInAppMessageImpression(IInAppMessage inAppMessage) {
+    if (inAppMessage != null) {
+      inAppMessage.logImpression();
+    } else {
+      Log.e(TAG, "The in-app message is null, Not logging in-app message impression.");
     }
+  }
 }
